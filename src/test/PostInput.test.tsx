d5d512bf30cd71edbb8b634afb714f619f5863f2
Arman Abbasian <a.abbasian@avaparsi.com>
2024-04-28 09:40:29 +0330

    work on PostInput test file

--- src/test/PostInput.test.tsx
+++ src/test/PostInput.test.tsx
@@ -2,35 +2,31 @@ import PostInput from "../components/PostInput";
 import { fireEvent, render, screen } from "../utils/test-utils";
 
 describe('PostInput', () => {
+  render(<PostInput />)
+  const titleInput = screen.getByLabelText('title') as HTMLInputElement;
+  const bodyInput = screen.getByLabelText('body') as HTMLInputElement;
+  const submitButton = screen.getByText('Add') as HTMLInputElement
     it('renders form elements correctly', () => {
-        render(<PostInput />)
-      expect(screen.getByLabelText('title')).toBeInTheDocument();
-      expect(screen.getByLabelText('body')).toBeInTheDocument();
-      expect(screen.getByText('Add')).toBeInTheDocument();
+      render(<PostInput />)  
+      expect(titleInput).toBeInTheDocument();
+      expect(bodyInput).toBeInTheDocument();
+      expect(submitButton).toBeInTheDocument();
     });
   
     it('updates form values on input change', () => {
         render(<PostInput />)
-      const titleInput = screen.getByLabelText('title') as HTMLInputElement;
-      const bodyInput = screen.getByLabelText('body') as HTMLInputElement;
-      
-  
       fireEvent.change(titleInput, { target: { value: 'Test Title' } });
       fireEvent.change(bodyInput, { target: { value: 'Test Body' } });
-  
       expect(titleInput.value).toBe('Test Title');
       expect(bodyInput.value).toBe('Test Body');
     });
   
     it('submits form with correct data', () => {
         render(<PostInput />)
-      const titleInput = screen.getByLabelText('title');
-      const bodyInput = screen.getByLabelText('body');
-      const addButton = screen.getByText('Add');
   
       fireEvent.change(titleInput, { target: { value: 'Test Title' } });
       fireEvent.change(bodyInput, { target: { value: 'Test Body' } });
-      fireEvent.click(addButton);
+      fireEvent.click(submitButton);
   
     });
   });
